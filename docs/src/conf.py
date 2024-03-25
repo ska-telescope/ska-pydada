375004961c8ed64a1ac4a8368e25efb1fa94b297
Will Gauvin <wgauvin@swin.edu.au>
2024-03-25 12:21:57 +1100

    AT3-683 fix docs not building in CI

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -89,6 +89,10 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
+autodoc_mock_imports = [
+    "numpy",
+    "nptyping",
+]
 
 # -- Options for HTML output ----------------------------------------------
 
